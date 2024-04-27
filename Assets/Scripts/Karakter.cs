f3100bf3f141948d49bd76273eb414773adad260
Ahmed Gürkan Özçelik <ahmetgozcelik@gmail.com>
2024-04-27 14:57:27 +0300

    Slider eklendi.

--- Assets/Scripts/Karakter.cs
+++ Assets/Scripts/Karakter.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Karakter : MonoBehaviour
 {
     public GameManager _GameManager;
     public Kamera _Kamera;
+    public Slider _Slider;
+
     public GameObject KarakterYeniPoz;
+    public GameObject GecisNoktasi;
 
     public bool SonaGelindiMiKarakter;
+
+    private void Start()
+    {
+        //Karakterin sona olan uzaklýðýný slider'ýn max value deðeri yap.
+        float fark = Vector3.Distance(transform.position, GecisNoktasi.transform.position);
+        _Slider.maxValue = fark;
+    }
     private void FixedUpdate()
     {
         if (SonaGelindiMiKarakter == false)
@@ -22,9 +33,19 @@ public class Karakter : MonoBehaviour
         if (SonaGelindiMiKarakter == true)
         {
             transform.position = Vector3.Lerp(transform.position, KarakterYeniPoz.transform.position, .015f);
+
+            //Slider'da kalan minik boþluðu doldurma iþlemi
+            if (_Slider.value != 0)
+            {
+                _Slider.value -= .004f;
+            }
         }
         else
         {
+            //Karakter ile son arasýndaki farký sürekli güncelle ve slider valuesine eþitle  
+            float fark = Vector3.Distance(transform.position, GecisNoktasi.transform.position);
+            _Slider.value = fark;
+
             if (Input.GetKey(KeyCode.Mouse0))
             {
                 if (Input.GetAxis("Mouse X") < 0)
